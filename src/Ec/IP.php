121576cdd36eb91abd4113a5052a9ba31e066f7b
Z.X <903464207@qq.com>
2022-05-27 21:34:27 +0800

    no commit message

---
+++ src/Ec/IP.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace ZX\Ec;
+
+class IP
+{
+
+
+    public function checkIp($ip)
+    {
+        $arr = explode('.', $ip);
+        if (count($arr) != 4) {
+            return false;
+        } else {
+            for ($i = 0; $i < 4; $i++) {
+                if (($arr[$i] < '0') || ($arr[$i] > '255')) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
