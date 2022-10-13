7a719395f6325afe6ac6d87ef1811aaa7765a05b
Z.X <903464207@qq.com>
2022-10-13 11:10:24 +0800

    删除不必要文件

--- tests/test.php
+++ tests/test.php
@@ -1,21 +1,29 @@
 <?php
 
-include_once './function.php';
-include_once './../src/Algorithm/LargeDigitalConversion62.php';
-include_once './../src/Ec/OrderKeyGenerator.php';
-include_once './../src/Ec/PageTool.php';
-include_once './../src/Ec/CheckTimeConflict.php';
-include_once './../src/Ec/IP.php';
-include_once './../src/Ec/File/MimeTypes.php';
-include_once './../src/Ec/File/SecurityCheck.php';
-include_once './../src/Ec/Id/GenerateIdCard.php';
-include_once './../src/Ec/Id/IdCard.php';
-include_once './../src/Ec/Id/IdCardData.php';
+include_once './../vendor/autoload.php';
 
 use ZX\Algorithm\LargeDigitalConversion62;
 use ZX\Ec\OrderKeyGenerator;
 use ZX\Ec\PageTool;
 use ZX\Ec\CheckTimeConflict;
+
+if (!function_exists('p')) {
+    function p($data = null)
+    {
+        echo '<pre>';
+        print_r($data);
+        echo '</pre>';
+    }
+}
+if (!function_exists('pp')) {
+    function pp($data = null)
+    {
+        echo '<pre>';
+        print_r($data);
+        echo '</pre>';
+        die;
+    }
+}
 
 //生成带前缀+时间序列+随机数的指定长度的订单号
 p(OrderKeyGenerator::getRandKey('UD_', 4));
@@ -35,7 +43,7 @@ p($data);
 $res = LargeDigitalConversion62::from10To62(123456);
 p($res);
 $res = LargeDigitalConversion62::from62To10($res);
-p($res);
+pp($res);
 
 //测试时间冲突
 $time = [];
